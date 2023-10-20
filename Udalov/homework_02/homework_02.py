e18e6a413a5819609463ecac7f1433f9b225c449
udalovpavel <upv.22@uni-dubna.ru>
2023-10-21 02:13:44 +0300

    udalov homework_02

--- Udalov/homework_02/homework_02.py
+++ Udalov/homework_02/homework_02.py
@@ -1,14 +1,16 @@
 def reverse(list):
-    b=[]
+    b = []
     for i in range(len(list)):
-        b.append(list-i-1)
+        b.append(list - i - 1)
     return b
 
+
 def avglen(list):
-    s=0
+    s = 0
     for i in list:
-        s+=len(i)
-    return s/len(list)
+        s += len(i)
+    return s / len(list)
+
 
 def index(arg):
     result = {}
@@ -23,28 +25,31 @@ def index(arg):
             result[i] = copy
     return result
 
+
 def coincidence(list1, list2):
-    b=[]
+    b = []
     for i in list1:
         for c in range(len(list2)):
-            if (i==list2[c]) and (i not in b):
+            if (i == list2[c]) and (i not in b):
                 b.append(i)
     return b
 
+
 def count(list):
-    dictionary={}
+    dictionary = {}
     for i in range(len(list)):
         if list[i] not in dictionary:
-            dictionary[list[i]]=1
+            dictionary[list[i]] = 1
         else:
-            dictionary[list[i]]+=1
+            dictionary[list[i]] += 1
     return dictionary
 
+
 def lensort(list):
-    for i in range(len(list)-1):
-        for j in range(i,len(list)):
-            if len(list[i])>len(list[j]):
+    for i in range(len(list) - 1):
+        for j in range(i, len(list)):
+            if len(list[i]) > len(list[j]):
                 temp = list[i]
-                list[i]=list[j]
-                list[j]=temp
+                list[i] = list[j]
+                list[j] = temp
     return list
