ebd5daaab0a6d5b3fe3e26d2dfc7b369907c419e
DariaKirova <83678549+DariaKirova@users.noreply.github.com>
2023-10-21 22:42:42 +0300

    Update homework_02.py

--- kirova/homework_01/homework_02.py
+++ kirova/homework_01/homework_02.py
@@ -1,28 +1,31 @@
-def reverse(list):
+def reverse(list1):
     new_l = []
-    for i in range(1, len(list) + 1):
-        new_l.append(list[-i]) 
+    for i in range(1, len(list1) + 1):
+        new_l.append(list1[-i]) 
     return new_l
 
-def avglen(list):
-    c=0
-    for word in list:
-        c=len(word)+c
-    return c/len(list)
 
-def index(list):
+def avglen(list1):
+    c=0
+    for word in list1:
+        c=len(word)+c
+    return c/len(list1)
+
+
+def index(list1):
     word_i={}
-    n=len(list)
+    n=len(list1)
     for i in range(n):
-        word=list[i]
+        word=list1[i]
         if word in word_i:
-            if type(word_i[word]) is list:
+            if type(word_i[word]) is list1:
                 word_i[word].append(i)
             else:
                 word_i[word] = [word_i[word], i]
         else:
             word_i[word] = i
     return word_i
+
 
 def coincidence(list1,list2):
     newlist=[]
@@ -31,20 +34,22 @@ def coincidence(list1,list2):
             if i==j:
                 newlist.append(i)
     return newlist
- 
-def count(list):
+
+
+def count(list1):
     word_i={}
-    for i in list:
+    for i in list1:
        if i in word_i:
             word_i[i] += 1
        else:
             word_i[i] = 1
     return word_i
 
-def lensort(list):
+
+def lensort(list1):
     diction = {}
     max_len = 0
-    for i in list:
+    for i in list1:
         n = len(i)
         if n in diction:
             diction[n].append(i)
@@ -56,4 +61,4 @@ def lensort(list):
         if i in diction:
             sort_list.extend(diction[i])
     return sort_list
- 
+    
