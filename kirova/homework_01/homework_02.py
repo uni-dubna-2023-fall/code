8e4053145dbbce2d456bf3761edf831d6d1c9d2a
DariaKirova <83678549+DariaKirova@users.noreply.github.com>
2023-10-21 22:58:59 +0300

    Update homework_02.py

--- kirova/homework_01/homework_02.py
+++ kirova/homework_01/homework_02.py
@@ -1,22 +1,22 @@
 def reverse(list1):
     new_l = []
     for i in range(1, len(list1) + 1):
-        new_l.append(list1[-i]) 
+        new_l.append(list1[-i])
     return new_l
 
 
 def avglen(list1):
-    c=0
+    c = 0
     for word in list1:
-        c=len(word)+c
-    return c/len(list1)
+        c = len(word) + c
+    return c / len(list1)
 
 
 def index(list1):
-    word_i={}
-    n=len(list1)
+    word_i = {}
+    n = len(list1)
     for i in range(n):
-        word=list1[i]
+        word = list1[i]
         if word in word_i:
             if type(word_i[word]) is list1:
                 word_i[word].append(i)
@@ -27,21 +27,21 @@ def index(list1):
     return word_i
 
 
-def coincidence(list1,list2):
-    newlist=[]
+def coincidence(list1, list2):
+    newlist = []
     for i in list1:
         for j in list2:
-            if i==j:
+            if i == j:
                 newlist.append(i)
     return newlist
 
 
 def count(list1):
-    word_i={}
+    word_i = {}
     for i in list1:
-       if i in word_i:
+        if i in word_i:
             word_i[i] += 1
-       else:
+        else:
             word_i[i] = 1
     return word_i
 
@@ -61,4 +61,3 @@ def lensort(list1):
         if i in diction:
             sort_list.extend(diction[i])
     return sort_list
-    
