248de46184f9b043ec97360ebebf6e17ccfee292
Aleksandr Sukhov <vacocs5@gmail.com>
2023-11-03 20:44:39 +0300

    end
    end2

--- sukhov/homework_03.py
+++ sukhov/homework_03.py
@@ -1,10 +1,16 @@
+import numpy
+
+
 class Node:
     def __init__(self, idx):
         self.idx = idx
-        self.phi = 0.0
+        self.phi = None
 
     def get_phi(self):
         return self.phi
+
+    def set_phi(self, phi):
+        self.phi = phi
 
 
 class Edge:
@@ -13,14 +19,14 @@ class Edge:
         self.r = r
         self.e = e
         self.j = j
-        self.tip_node = None
-        self.tail_node = None
+        self.tip = None
+        self.tail = None
 
     def attach_tip(self, node):
-        self.tip_node = node
+        self.tip = node
 
     def attach_tail(self, node):
-        self.tail_node = node
+        self.tail = node
 
 
 class Circuit:
@@ -35,24 +41,30 @@ class Circuit:
         self.edges.append(edge)
 
     def solve(self):
-        n = len(self.nodes)
-        A = [[0.0 for _ in range(n)] for _ in range(n)]
-        B = [0.0 for _ in range(n)]
+        len_nodes = len(self.nodes)
+
+        A = [[0.0] * len_nodes for i in range(len_nodes)]
+        b = [0.0] * len_nodes
 
         for edge in self.edges:
-            if edge.tip_node and edge.tail_node:
-                i = edge.tip_node.idx
-                j = edge.tail_node.idx
-                A[i][i] += 1 / edge.r
-                A[j][j] += 1 / edge.r
-                A[i][j] -= 1 / edge.r
-                A[j][i] -= 1 / edge.r
-                B[i] += edge.e
-                B[j] -= edge.e
+            r = edge.r
+            edge_e = edge.e - edge.j * r
 
-        phi = [0.0] * n
-        for i in range(n):
-            phi[i] = B[i] / A[i][i]
+            if edge.tip is not None:
+                tip_idx = edge.tip.idx
+                A[tip_idx][tip_idx] += 1 / r
+                b[tip_idx] += edge_e / r
 
-        for node in self.nodes:
-            node.phi = phi[node.idx]
+            if edge.tail is not None:
+                tail_idx = edge.tail.idx
+                A[tail_idx][tail_idx] += 1 / r
+                b[tail_idx] -= edge_e / r
+
+            if edge.tip is not None and edge.tail is not None:
+                A[tip_idx][tail_idx] -= 1 / r
+                A[tail_idx][tip_idx] -= 1 / r
+
+        self.phi = numpy.linalg.solve(A, b)
+
+        for i in range(len_nodes):
+            self.nodes[i].set_phi(self.phi[i])
