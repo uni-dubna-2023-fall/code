51570c2473ce91917e5e71cddee70cc8a0e2c49a
Kolesin Alexandr <kolesin2604@gmail.com>
2023-11-04 23:15:22 +0300

    kolesin homework 03

--- kolesin/homework_03.py
+++ kolesin/homework_03.py
@@ -1,95 +1,72 @@
-class Node:
-    def __init__(self, index):
-        self.index = index
-        self.edges = []
-        self.phi = 0.0
+import numpy as np
 
-    def attach_edge(self, edge):
-        self.edges.append(edge)
+class Node:
+    def __init__(self, idx):
+        self.idx = idx
+        self.phi = None
+        self.edges = {}
+    
+    def attach(self, edge, direction):
+        self.edges[edge.idx] = [edge, direction]
 
 class Edge:
-    def __init__(self, index, resistance, current=0.0):
-        self.index = index
-        self.resistance = resistance
-        self.current = current
+    def __init__(self, idx, r, e=None, j=None):
+        self.idx = idx
+        self.r = r
+        self.e = e
+        self.j = j
+        self.y = 1.0 / self.r
         self.tip = None
         self.tail = None
-
+    
     def attach_tip(self, node):
         self.tip = node
-        node.attach_edge(self)
-
+        node.attach(self, -1.0)
+    
     def attach_tail(self, node):
         self.tail = node
-        node.attach_edge(self)
+        node.attach(self, 1.0)
 
 class Circuit:
     def __init__(self):
-        self.nodes = []
-        self.edges = []
-
+        self.nodes = {}
+        self.edges = {}
+        self.A = None
+        self.Y = None
+        self.J = None
+        self.E = None
+    
     def add_node(self, node):
-        self.nodes.append(node)
-
+        self.nodes[node.idx] = node
+    
     def add_edge(self, edge):
-        self.edges.append(edge)
-
-    def solve(self):
-        # Initialize potentials at each node
-        for node in self.nodes:
-            node.phi = 0.0
-
-        # Iterate until convergence
-        max_iterations = 1000
-        tolerance = 1e-6
-        for iteration in range(max_iterations):
-            max_error = 0.0
-            
-            # Update potentials at each node
-            for node in self.nodes:
-                prev_phi = node.phi
-                if len(node.edges) > 0:
-                    total_current = sum(edge.current for edge in node.edges)
-                    total_resistance = sum(edge.resistance for edge in node.edges)
-                    if total_resistance > 0:
-                        node.phi = total_current / total_resistance
-                error = abs(node.phi - prev_phi)
-                max_error = max(max_error, error)
-            
-            # Check for convergence
-            if max_error < tolerance:
-                break
-
-    def get_phi(self):
-        return [node.phi for node in self.nodes]
-        
-n0 = Node(0)
-n1 = Node(1)
-
-e0 = Edge(0, 100.0)
-e1 = Edge(1, 100.0)
-e2 = Edge(2, 100.0)
-e3 = Edge(3, 100.0)
-e4 = Edge(4, 100.0, 5.0)
-
-e0.attach_tip(n0)
-e1.attach_tip(n0)
-e2.attach_tip(n1)
-e3.attach_tip(n1)
-e3.attach_tail(n0)
-e4.attach_tail(n1)
-
-c = Circuit()
-c.add_node(n0)
-c.add_node(n1)
-
-c.add_edge(e0)
-c.add_edge(e1)
-c.add_edge(e2)
-c.add_edge(e3)
-c.add_edge(e4)
-
-c.solve()
-
-print(f"\u03C6_0 = {c.get_phi()[0]}")
-print(f"\u03C6_1 = {c.get_phi()[1]}")
+        self.edges[edge.idx] = edge
+    
+    def _calculate_A(self):
+        num_nodes = len(self.nodes)
+        num_edges = len(self.edges)
+        self.A = np.zeros((num_nodes, num_edges))
+        for i, node_idx in enumerate(self.nodes):
+            for j, edge_idx in enumerate(self.edges):
+                if edge_idx in self.nodes[node_idx].edges:
+                    self.A[i, j] = self.nodes[node_idx].edges[edge_idx][1]
+    
+    def _calculate_Y(self):
+        num_edges = len(self.edges)
+        self.Y = np.zeros((num_edges, num_edges))
+        for edge_idx in self.edges:
+            self.Y[edge_idx, edge_idx] = self.edges[edge_idx].y
+    
+    def _calculate_J(self):
+        num_nodes = len(self.nodes)
+        self.J = np.zeros((num_nodes, 1))
+        for node_idx in self.nodes:
+            if self.nodes[node_idx].phi is not None:
+                self.J[node_idx, 0] = -self.nodes[node_idx].phi
+    
+    def _calculate_E(self):
+        num_edges = len(self.edges)
+        self.E = np.zeros((num_edges, 1))
+        for edge_idx in self.edges:
+            if self.edges[edge_idx].e is not None:
+                self.E[edge_idx, 0] = self.edges[edge_idx].e
