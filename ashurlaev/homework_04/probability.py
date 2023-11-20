8d46738599c6c80297f2ae76e054d0d75fe00d0a
Arslan Ashurlaev <ashurlaev.arslan@gmail.com>
2023-11-20 23:39:54 +0300

    ashurlaev homework_04

--- ashurlaev/homework_04/probability.py
+++ ashurlaev/homework_04/probability.py
@@ -37,3 +37,4 @@ class ProbabilityMoments:
                          x * math.sin(radians) + y * math.cos(radians))
                         for x, y in self.data]
         self.data = rotated_data
+moments = ProbabilityMoments()
