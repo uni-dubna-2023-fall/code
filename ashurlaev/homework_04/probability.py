c022b5d306a0f16ef716dc05472bd7e192552fbd
Arslan Ashurlaev <ashurlaev.arslan@gmail.com>
2023-11-21 00:47:51 +0300

    ashurlaev homework_04

--- ashurlaev/homework_04/probability.py
+++ ashurlaev/homework_04/probability.py
@@ -3,35 +3,36 @@ import os
 
 
 class ProbabilityMoments:
-    def __init__(self, file_path):
-        self.file_path = file_path
+    def __init__(self, fn):
+        self.fn = fn
         self.data = []
 
     def __enter__(self):
-        if os.path.exists(self.file_path):
-            with open(self.file_path, 'r') as file_handle:
-                self.data = json.load(file_handle)
+        if os.path.exists(self.fn):
+            with open(self.fn, 'r') as file:
+                self.data = json.load(file)
         return self
 
     def __exit__(self, *exc_info):
-        with open(self.file_path, 'w') as file_handle:
-            json.dump(self.data, file_handle)
+        with open(self.fn, 'w') as file
+            json.dump(self.data, file)
 
     def append_data(self, new_data):
         self.data.append(new_data)
 
     def calculate_mean(self):
-        if self.data:
+        if not self.data:
+           return None
+        else:
             total_sum = sum(self.data)
             num_points = len(self.data)
             return total_sum / num_points
-        else:
-            return None
+            
 
     def calculate_std_dev(self):
         if len(self.data) >= 2:
             mean_value = self.calculate_mean()
-            var_sum = sum((point - mean_value) ** 2 for point in self.data)
-            return (var_sum / len(self.data)) ** 0.5
+            sum = sum((point - mean_value) ** 2 for point in self.data)
+            return ((sum / len(self.data))** 0.5)
         else:
             return 0
