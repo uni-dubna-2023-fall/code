923635b9d66ef0ee4cc8c53f8f75e36de343f4ae
Arslan Ashurlaev <ashurlaev.arslan@gmail.com>
2023-11-21 01:41:31 +0300

    ashurlaev homework_04

--- ashurlaev/homework_04/probability.py
+++ ashurlaev/homework_04/probability.py
@@ -5,7 +5,7 @@ class ProbabilityMoments:
     def __init__(self, filename):
         self.filename = filename
         self.data = []
-
+    
     def __enter__(self):
         try:
             with open(self.filename, 'r') as file:
@@ -17,19 +17,19 @@ class ProbabilityMoments:
     def __exit__(self, *args):
         with open(self.filename, 'w') as file:
             json.dump(self.data, file)
-
+    
     def add(self, x):
         self.data.append(x)
-
+    
     def mean(self):
         if self.data:
             return sum(self.data) / len(self.data)
         else:
-            None
-
+            return None
+    
     def variance(self):
         mu = self.mean()
         if self.data:
-            return (sum((x - mu) ** 2 for x in self.data)) / len(self.data)
+            return sum((x - mu) ** 2 for x in self.data) / len(self.data)
         else:
-            None
+            return None
