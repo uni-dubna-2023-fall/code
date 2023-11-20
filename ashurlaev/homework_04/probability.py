558426b3e5300777cec9e16c3cb2c2b2c9f1e689
Arslan Ashurlaev <ashurlaev.arslan@gmail.com>
2023-11-20 23:35:53 +0300

    ashurlaev homework_04

--- ashurlaev/homework_04/probability.py
+++ ashurlaev/homework_04/probability.py
@@ -3,36 +3,37 @@ import math
 
 class ProbabilityMoments:
     def __init__(self):
-        self.d = []
+        self.data = []
 
     def add(self, x, y):
-        self.d.append((x, y))
+        self.data.append((x, y))
 
     def mean(self):
-        if not self.d:
+        if not self.data:
             return None
-        sum_x = sum(x for x, _ in self.d)
-        sum_y = sum(y for _, y in self.d)
-        return sum_x / len(self.d), sum_y / len(self.d)
+        sum_x = sum(x for x, _ in self.data)
+        sum_y = sum(y for _, y in self.data)
+        return sum_x / len(self.data), sum_y / len(self.data)
 
     def variance(self):
-        if len(self.d) < 2:
+        if len(self.data) < 2:
             return 0
         mean_x, mean_y = self.mean()
-        var_x = sum((x - mean_x) ** 2 for x, _ in self.d) / len(self.d)
-        var_y = sum((y - mean_y) ** 2 for _, y in self.d) / len(self.d)
+        var_x = sum((x - mean_x) ** 2 for x, _ in self.data) / len(self.data)
+        var_y = sum((y - mean_y) ** 2 for _, y in self.data) / len(self.data)
         return var_x, var_y
 
     def shift(self, dx, dy):
-        shifted_d = [(x + dx, y + dy) for x, y in self.d]
-        self.d = shifted_d
+        shifted_data = [(x + dx, y + dy) for x, y in self.data]
+        self.data = shifted_data
 
     def reflect(self):
-        reflected_d = [(-x, -y) for x, y in self.d]
-        self.d = reflected_d
+        reflected_data = [(-x, -y) for x, y in self.data]
+        self.data = reflected_data
 
     def rotate(self, angle):
         radians = math.radians(angle)
-        rotated_d = [(x * math.cos(radians) - y * math.sin(radians),
-                    x * math.sin(radians) + y * math.cos(radians)) for x, y in self.d]
-        self.d = rotated_d
+        rotated_data = [(x * math.cos(radians) - y * math.sin(radians),
+                         x * math.sin(radians) + y * math.cos(radians)) 
+                         for x, y in self.data]
+        self.data = rotated_data
