9d0aec851e0a73ea927443d5976813537bf58266
Arslan Ashurlaev <ashurlaev.arslan@gmail.com>
2023-11-20 23:59:44 +0300

    ashurlaev homework_04

--- ashurlaev/homework_04/probability.py
+++ ashurlaev/homework_04/probability.py
@@ -33,9 +33,10 @@ class ProbabilityMoments:
 
     def rotate(self, angle):
         radians = math.radians(angle)
-        rotated_data = [(x * math.cos(radians) - y * math.sin(radians), 
-                         x * math.sin(radians) + y * math.cos(radians)) 
-                         for x, y in self.data]
+        rotated_data = [(x * math.cos(radians) - y * math.sin(radians),
+                         x * math.sin(radians) + y * math.cos(radians))
+                        for x, y in self.data]
         self.data = rotated_data
+
 
 probability_moments = ProbabilityMoments()
