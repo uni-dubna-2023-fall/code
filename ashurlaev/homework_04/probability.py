c26d1c3e120bdbc7258302eee88adc378537df4b
Arslan Ashurlaev <ashurlaev.arslan@gmail.com>
2023-11-21 01:09:13 +0300

    ashurlaev homework_04

--- ashurlaev/homework_04/probability.py
+++ ashurlaev/homework_04/probability.py
@@ -3,31 +3,33 @@ import os
 
 
 class ProbabilityMoments:
-    def __init__(self, path):
-        self.path = path
-        self.numbers = []
+    def __init__(self, fn):
+        self.fn = fn
+        self.data = []
 
     def __enter__(self):
-        if os.path.isfile(self.path):
-            with open(self.path, 'r') as f:
-                self.numbers = json.load(f)
+        if os.path.exists(self.fn):
+            with open(self.fn, 'r') as file:
+                self.data = json.load(file)
         return self
 
-    def __exit__(self, *exc):
-        with open(self.path, 'w') as f:
-            json.dump(self.numbers, f)
+    def __exit__(self, *exc_info):
+        with open(self.fn, 'w') as file:
+            json.dump(self.data, file)
 
-    def insert(self, value):
-        self.numbers.append(value)
+    def append(self, new_data):
+        self.data.append(new_data)
 
-    def average(self):
-        if self.numbers:
-            return sum(self.numbers) / len(self.numbers)
-        return None
+    def calculate(self):
+        if self.data:
+            return sum(self.data) / len(self.data)
+        else:
+            None
 
-    def deviation(self):
-        if len(self.numbers) >= 2:
-            avg = self.average()
-            return (sum((n - avg) ** 2 for n in self.numbers)
-                    / len(self.numbers)) ** 0.5
-        return 0
+    def calculate(self):
+        if len(self.data) < 2:
+            return 0   
+        mean_value = self.calculate()
+        summa = sum((h - mean_value) ** 2 for h in self.data)
+        return ((summa / len(self.data)) ** 0.5)
+        
