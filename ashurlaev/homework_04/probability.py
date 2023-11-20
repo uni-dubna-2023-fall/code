70b4b4827ff9a874c0b6a08385e315ba0ec3e9cd
Arslan Ashurlaev <ashurlaev.arslan@gmail.com>
2023-11-20 23:29:26 +0300

    ashurlaev homework_04

--- ashurlaev/homework_04/probability.py
+++ ashurlaev/homework_04/probability.py
@@ -1,39 +1,44 @@
 import math
 
 
-def shift(dx, dy):
-    def decorator(func):
-        def wrapper(*args):
-            a_args = []
-            for x, y in args:
-                shifted_x = x + dx
-                shifted_y = y + dy
-                a_args.append((shifted_x, shifted_y))
-            return func(*a_args)
-        return wrapper
-    return decorator
+class ProbabilityMoments:
+    def __init__(self):
+        self.data = []
 
 
-def reflect(func):
-    def wrapper(*args):
-        reflected_args = []
-        for x, y in args:
-            reflected_x = -x
-            reflected_y = -y
-            reflected_args.append((reflected_x, reflected_y))
-        return func(*reflected_args)
-    return wrapper
+    def add(self, x, y):
+        self.data.append((x, y))
 
 
-def rotate(angle):
-    def decorator(func):
-        def wrapper(*args):
-            radians = math.radians(angle)
-            povorot_args = []
-            for x, y in args:
-                rotated_x = x * math.cos(radians) - y * math.sin(radians)
-                rotated_y = x * math.sin(radians) + y * math.cos(radians)
-                povorot_args.append((rotated_x, rotated_y))
-            return func(*povorot_args)
-        return wrapper
-    return decorator
+    def mean(self):
+        if not self.data:
+            return None
+        sum_x = sum(x for x, _ in self.data)
+        sum_y = sum(y for _, y in self.data)
+        return sum_x / len(self.data), sum_y / len(self.data)
+
+
+    def variance(self):
+        if len(self.data) < 2:
+            return 0
+        mean_x, mean_y = self.mean()
+        var_x = sum((x - mean_x) ** 2 for x, _ in self.data) / len(self.data)
+        var_y = sum((y - mean_y) ** 2 for _, y in self.data) / len(self.data)
+        return var_x, var_y
+
+
+    def shift(self, dx, dy):
+        shifted_data = [(x + dx, y + dy) for x, y in self.data]
+        self.data = shifted_data
+
+
+    def reflect(self):
+        reflected_data = [(-x, -y) for x, y in self.data]
+        self.data = reflected_data
+
+
+    def rotate(self, angle):
+        radians = math.radians(angle)
+        rotated_data = [(x * math.cos(radians) - y * math.sin(radians),
+                         x * math.sin(radians) + y * math.cos(radians)) for x, y in self.data]
+        self.data = rotated_data
