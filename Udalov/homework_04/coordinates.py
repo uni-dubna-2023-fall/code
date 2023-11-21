1e35eeb93b81430d0d749b417c3aefe5fda3821e
udalovpavel <upv.22@uni-dubna.ru>
2023-11-21 22:21:52 +0300

    udalov homework_04

--- Udalov/homework_04/coordinates.py
+++ Udalov/homework_04/coordinates.py
@@ -7,7 +7,7 @@ def shift(dx, dy):
             new_args = []
             for x, y in args:
                 new_args.append((x + dx, y + dy))
-            return func(new_args)
+            return func(*new_args)
         return wrapper
     return decorator
 
@@ -29,6 +29,6 @@ def rotate(phi):
             sin = math.sin(math.radians(phi))
             for x, y in args:
                 new_args.append((x * cos - y * sin, x * sin + y * cos))
-            return func(new_args)
+            return func(*new_args)
         return wrapper
     return sec_decorator
