9a186297a298431b27f14d159cf994e0b86bb740
Kalashnikov_Artem <Artikyl32@gmail.com>
2023-12-06 18:23:20 +0300

    Fixes HW2

--- kalashnikov/homework_02.py
+++ kalashnikov/homework_02.py
@@ -1,9 +1,11 @@
 def reverse(lst):
     return lst[::-1]
 
+
 arg = ["my", "name", "is", "Masha"]
 result = reverse(arg)
 print("1)", result, "\n")
+
 
 
 def avglen(words):
@@ -11,9 +13,11 @@ def avglen(words):
     average_length = total_length / len(words)
     return average_length
 
+
 arg = ["a", "ab", "abc"]
 result = avglen(arg)
 print("2)", result, "\n")
+
 
 
 def index(words):
@@ -25,7 +29,9 @@ def index(words):
             index_dict[word] = [i]
     return index_dict
 
-arg = ["her", "name", "is", "Masha", "Masha", "is", "a", "sister", "of", "Zhenya"]
+
+arg = ["her", "name", "is", "Masha", "Masha", 
+       "is", "a", "sister", "of", "Zhenya"]
 result = index(arg)
 print("3)")
 for word, indexes in result.items():
@@ -33,13 +39,16 @@ for word, indexes in result.items():
 print("\n")
 
 
+
 def coincidence(list1, list2):
     return list(set(list1) & set(list2))
+
 
 arg1 = ["my", "name", "is", "Masha"]
 arg2 = ["my", "name", "is", "Zhenya"]
 result = coincidence(arg1, arg2)
 print("4)", result, "\n")
+
 
 
 def count(words):
@@ -48,13 +57,16 @@ def count(words):
         counts[word] = counts.get(word, 0) + 1
     return counts
 
+
 arg = ["aaa", "aaa", "bbb", "ccc", "bbb"]
 result = count(arg)
 print("5)", result, "\n")
 
 
+
 def lensort(words):
     return sorted(words, key=len)
+
 
 arg = ["abcd", "a", "ab", "abc", "bazinga", "bar"]
 result = lensort(arg)
