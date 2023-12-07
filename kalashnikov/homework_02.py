1358a3c4f93e2f4269f0140b3599ea0082fbaeea
Play Art <Artikyl32@gmail.com>
2023-12-07 22:00:56 +0300

    Fixes

--- kalashnikov/homework_02.py
+++ kalashnikov/homework_02.py
@@ -2,20 +2,10 @@ def reverse(lst):
     return lst[::-1]
 
 
-arg = ["my", "name", "is", "Masha"]
-result = reverse(*arg)
-print("2) ", result, "\n")
-
-
 def avglen(words):
     total_length = sum(len(word) for word in words)
     average_length = total_length / len(words)
     return average_length
-
-
-arg = ["a", "ab", "abc"]
-result = avglen(arg)
-print("3) ", result, "\n")
 
 
 def index(words):
@@ -28,28 +18,9 @@ def index(words):
     return index_dict
 
 
-arg = ["her", "name", "is", "Masha", "Masha",
-       "is", "a", "sister", "of", "Zhenya"]
-result = index(arg)
-print("4)")
-for word, indexes in result.items():
-    print(f"{word}: {indexes}")
-
-
 def coincidence(list1, list2):
     return list(set(list1) & set(list2))
 
 
-arg1 = ["my", "name", "is", "Masha"]
-arg2 = ["my", "name", "is", "Zhenya"]
-result = coincidence(arg1, arg2)
-print("\n5) ", result, "\n")
-
-
 def lensort(words):
     return sorted(words, key=len)
-
-
-arg = ["abcd", "a", "ab", "abc", "bazinga", "bar"]
-result = lensort(arg)
-print("6) ", result)
