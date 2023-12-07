65771fd5932f760a952dcf7f66a7e9a2238a2bbf
Play Art <Artikyl32@gmail.com>
2023-12-08 00:30:02 +0300

    Fix 3

--- kalashnikov/homework_03.py
+++ kalashnikov/homework_03.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self, node_id):
         self.node_id = node_id
         self.phi = 0.0
-    
+
     def get_phi(self):
         return self.phi
 
@@ -13,35 +13,36 @@ class Edge:
         self.resistance = resistance
         self.tip = None
         self.tail = None
-        
+
     def attach_tip(self, node):
         self.tip = node
-        
+
     def attach_tail(self, node):
         self.tail = node
 
     def get_current(self):
         return (self.tip.get_phi() - self.tail.get_phi()) / self.resistance
-        
+
     def get_emf(self):
         return self.get_current() * self.resistance
-    
+
 
 class Circuit:
     def __init__(self):
         self.nodes = []
         self.edges = []
-        
+
     def add_node(self, node):
         self.nodes.append(node)
-        
+
     def add_edge(self, edge):
         self.edges.append(edge)
-        
+
     def solve(self):
         for edge in self.edges:
             edge.tip.phi = edge.tail.phi + edge.get_emf()
-    
+
     def print_node_potentials(self):
         for node in self.nodes:
             print(f"\u03C6_{node.node_id} = {node.get_phi()}")
+            
