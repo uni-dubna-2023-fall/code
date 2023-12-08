800b16bb653b499652465f31b2f98d4e1fcb4011
macbook <macbook@MacBook-Air-macbook.local>
2023-12-08 14:24:50 +0300

    homework_05

---
+++ karavaev/homework_05.py
@@ -0,0 +1,109 @@
+from flask import Flask, request, jsonify
+import numpy as np
+from concurrent.futures import ProcessPoolExecutor
+
+app = Flask(__name__)
+
+
+class Manager:
+    def __init__(self):
+        self.tasks = {}
+        self.task_counter = 0
+        self.executor = ProcessPoolExecutor()
+
+    def stop(self):
+        self.executor.shutdown()
+
+    def multiply_matrices(self, matrices):
+        matrix_a, matrix_b = matrices
+        result_shape = (matrix_a.shape[0], matrix_b.shape[1])
+        result = np.zeros(result_shape)
+
+        chunk_size = result_shape[0] // len(list(self.executor._processes))
+        chunks = []
+        for i in range(len(list(self.executor._processes)) - 1):
+            chunks.append((i * chunk_size, (i + 1) * chunk_size))
+        last_chunk_start = (len(list(self.executor._processes)) - 1) * chunk_size
+        last_chunk_end = result_shape[0]
+        chunks.append((last_chunk_start, last_chunk_end))
+
+        futures = [self.executor.submit(np.dot, matrix_a[start:end, :], matrix_b) for start, end in chunks]
+        results = [future.result() for future in futures]
+
+        for i, (start, end) in enumerate(chunks):
+            result[start:end, :] = results[i]
+
+        return result.tolist()
+
+    def add_task(self, matrix_a, matrix_b):
+        task_id = str(self.task_counter)
+        self.tasks[task_id] = {
+            'status': 'running',
+            'result': None,
+            'task': self.executor.submit(self.multiply_matrices, (matrix_a, matrix_b))
+        }
+        self.task_counter += 1
+        return task_id
+
+    def status_task(self, task_id):
+        task = self.tasks.get(task_id)
+        if task:
+            if task['task'].done():
+                task['status'] = 'finished'
+                task['result'] = task['task'].result()
+            return {'task_id': task_id, 'status': task['status']}
+        else:
+            return {'error': 'Task not found'}
+
+    def get_result(self, task_id):
+        task = self.tasks.get(task_id)
+        if task and task['status'] == 'finished':
+            return {'task_id': task_id, 'result': task['result']}
+        else:
+            return {'error': 'Task not finished or not found'}
+
+    def remove_task(self, task_id):
+        if task_id in self.tasks:
+            del self.tasks[task_id]
+            return {'task_id': task_id, 'status': 'ok'}
+        else:
+            return {'error': 'Task not found'}
+
+    def list_tasks(self):
+        return list(self.tasks.keys())
+
+
+manager = Manager()
+
+
+@app.route('/multiply', methods=['POST'])
+def multiply():
+    data = request.json
+    matrix_a = np.array(data['matrix_a'])
+    matrix_b = np.array(data['matrix_b'])
+    task_id = manager.add_task(matrix_a, matrix_b)
+    return jsonify({'task_id': task_id, 'status': 'running'})
+
+
+@app.route('/status/<task_id>', methods=['GET'])
+def status_task(task_id):
+    return jsonify(manager.status_task(task_id))
+
+
+@app.route('/tasks', methods=['GET'])
+def list_task():
+    return jsonify(manager.list_tasks())
+
+
+@app.route('/result/<task_id>', methods=['GET'])
+def get_result(task_id):
+    return jsonify(manager.get_result(task_id))
+
+
+@app.route('/remove/<task_id>', methods=['DELETE'])
+def remove_task(task_id):
+    return jsonify(manager.remove_task(task_id))
+
+
+if __name__ == '__main__':
+    app.run()
